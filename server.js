bd1ba9033f0367f3658ae67859028e9679422a14
RoushanSri <roshansrivastav59796@gmail.com>
2025-07-20 22:11:51 +0530

    add .env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import { app, server, io } from "./config/socket.js";
+import { config } from "dotenv";
 import express from "express"
 import path from "path";
 import { fileURLToPath } from "url";
@@ -7,6 +8,8 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+config()
+
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "Public")));
 
@@ -14,6 +17,6 @@ app.get("/", (_, res)=>{
     res.render("index")
 })
 
-server.listen(8080, ()=>{
+server.listen(process.env.PORT, ()=>{
     console.log("Server Started on port 8080!!");
 })
